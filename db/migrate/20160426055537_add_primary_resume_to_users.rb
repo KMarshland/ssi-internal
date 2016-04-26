76d8625bbd4bd7fb7ed4efc9d1ed513c60b3ad25
Kai Marshland <kaimarshland@gmail.com>
2016-04-25 23:13:54 -0700

    Theoretically fixed migration

--- db/migrate/20160426055537_add_primary_resume_to_users.rb
+++ db/migrate/20160426055537_add_primary_resume_to_users.rb
@@ -1,6 +1,5 @@
 class AddPrimaryResumeToUsers < ActiveRecord::Migration
   def change
     add_reference :users, :primary_resume, index: true
-    add_foreign_key :users, :primary_resumes
   end
 end
