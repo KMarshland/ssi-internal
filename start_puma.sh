fe48058466a68f68d7243f5855c9827c3aecde35
Kai Marshland <kaimarshland@gmail.com>
2016-05-04 03:05:45 -0700

    Config for AWS

--- start_puma.sh
+++ start_puma.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-source ~/.bashrc
-source ~/.profile
-cd /var/internal && /root/.rbenv/shims/bundle exec puma -C config/puma.rb -e production
+printf "Trying to boot the bastard\n" >> /home/ubuntu/internal/log/upstart.log
+source /home/ubuntu/.bashrc
+source /home/ubuntu/.profile
+source /home/ubuntu/.bash_profile
+cd /home/ubuntu/internal && /home/ubuntu/.rbenv/shims/bundle exec /home/ubuntu/.rbenv/shims/puma -C config/puma.rb -e production
